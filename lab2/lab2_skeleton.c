0c1f0d4c3f923bf4439ce181d53ad59e9606a954
Caleb Kemere <ckemere@gmail.com>
2018-01-12 11:37:46 -0600

    Updated Lab2 skeleton code

--- lab2/lab2_skeleton.c
+++ lab2/lab2_skeleton.c
@@ -14,40 +14,60 @@
 
 #include <msp430g2553.h>		//must include so compiler knows what each variable means
 
-int next_led();
+#define CLOCKWISE 1
+#define COUNTERCLOCKWISE 0
+
+
+/* -------------------------------------------- *
+   next_led()
+
+   Returns:
+    - unsigned int - lower byte maps to Port 2 pins
+        and upper byte maps to Port 1 pins.
+ * -------------------------------------------- */
+
+unsigned int next_led()
+{
+  static int led_value = 0x01;  // What does static mean?
+  static int direction = CLOCKWISE;
+
+  if (direction == CLOCKWISE) {
+  }
+  else {
+  }
+}
+
 
 void main(void){
 	WDTCTL = WDTPW + WDTHOLD;	//Stop WDT
-	CCTL0 = CCIE;				//Puts the timer control on CCR0
-	CCR0 = 1000;				//1000 cycles until first interrupt
-	TACTL = TASSEL_2 + MC_2;	//right click and "Open Declaration" to find out about each variable
-                            //(adding a specific setting applies it to that control variable)
+  BCSCTL1 = CALBC1_1MHZ;    // Set range
+  DCOCTL = CALDCO_1MHZ;     // Set DCO step + modulation */
+  BCSCTL2 |= ;              // HINT - you probably want to divide the appropriate clock by 8 to
+                            //    make it as slow as possible.
 
-  P1DIR |= BIT2; 
-  P2DIR |= BIT2; 
-  P1OUT &= ~(BIT2);
-  P2OUT &= ~(BIT2);
+
+	CCTL0 = CCIE;				//Puts the timer control on CCR0
+	CCR0 = 1000;				// 1000 cycles until first interrupt
+	TACTL = TASSEL_2 + MC_1;	//right click and "Open Declaration" to find out about each variable
+                            //(adding a specific setting applies it to that control variable)
+                            // HINT - you may want to add a divider for the clock!
+
+  P1DIR |= ;      // Refer to EAGLE cad file to set up the approriate pins
+  P2DIR |= 0x01;  // This line must be changed!
+
+  P1OUT = 0;  // Initialize all outputs to 0
+  P2OUT = 0;
 
 	__enable_interrupt();		//global interrupt enable
 
+
+  unsigned int output;
+
   while (1) {
-    switch (next_led()) {
-      case 0:
-        break;
-      case 1:
-        break;
-      case 2:
-        break;
-      case 3:
-        break;
-      case 4:
-        break;
-      case 5:
-        break;
-      default:
-        break;
-    }
-  __bis_SR_register(CPUOFF + GIE);
+    __bis_SR_register(CPUOFF + GIE);
+    output = next_led(direction);
+    P1OUT = output && 0xFF00;  // There is an error in this line
+    P2OUT = output && 0x00FF;
   }
 }
 
